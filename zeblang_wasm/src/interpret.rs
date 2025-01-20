b32e7353aa281cd33505d9e254bdef9220be31b2
zeb33n <zeb.bond@33n.co.uk>
2025-01-20 19:54:29 +0000

    nice code bro

--- zeblang_wasm/src/interpret.rs
+++ zeblang_wasm/src/interpret.rs
@@ -1,3 +1,52 @@
-use zeblang::StatementNode;
+use zeblang::{ExpressionNode, StatementNode};
 
-pub fn interpret(parse_tree: Vec<StatementNode>) -> () {}
+struct Interpreter {}
+
+pub fn interpret(parse_tree: Vec<StatementNode>) -> Result<i32, String> {
+    return Interpreter {}.run(parse_tree);
+}
+
+impl Interpreter {
+    fn run(&mut self, parse_tree: Vec<StatementNode>) -> Result<i32, String> {
+        let mut out = Err("Error".to_string());
+        for statement in parse_tree.into_iter() {
+            match statement {
+                StatementNode::Exit(node) => {
+                    out = self.interpret_exit(node);
+                    break;
+                }
+                StatementNode::Assign(_, _) => todo!(),
+                _ => todo!(),
+            }
+        }
+        return out;
+    }
+
+    fn interpret_exit(&mut self, node: ExpressionNode) -> Result<i32, String> {
+        let value = self.interpret_expr(node);
+        return value;
+    }
+
+    fn interpret_expr(&mut self, node: ExpressionNode) -> Result<i32, String> {
+        return match node {
+            ExpressionNode::Value(value) => {
+                value.parse::<i32>().ok().ok_or("invalid int".to_string())
+            }
+            ExpressionNode::Infix(node1, infix, node2) => {
+                let v1 = self.interpret_expr(*node1)?;
+                let v2 = self.interpret_expr(*node2)?;
+                match infix.as_str() {
+                    "+" => Ok(v1 + v2),
+                    "-" => Ok(v1 - v2),
+                    "*" => Ok(v1 * v2),
+                    "/" => Ok(v1 / v2),
+                    "==" => Ok((v1 == v2) as i32),
+                    "!=" => Ok((v1 != v2) as i32),
+                    "%" => Ok(v1 % v2),
+                    _ => Err("Invalid Infix op".to_string()),
+                }
+            }
+            _ => todo!(),
+        };
+    }
+}
--- zeblang_wasm/src/lib.rs
+++ zeblang_wasm/src/lib.rs
@@ -2,6 +2,7 @@ use wasm_bindgen::prelude::*;
 
 use zeblang;
 mod interpret;
+use interpret::interpret;
 
 #[wasm_bindgen]
 extern "C" {
@@ -16,7 +17,7 @@ pub fn greet(name: &str) {
 #[wasm_bindgen]
 pub fn do_compile(src: &str) {
     match zeblang::make_parsetree(src.to_string()) {
-        Ok(out) => alert(&format!("{:?}", out)),
+        Ok(out) => alert(&format!("{}", interpret(out).unwrap())),
         Err(_) => alert("error"),
     }
 }
