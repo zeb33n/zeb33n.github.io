daa2ddb7401e5663c3a0674576026638ea9dfecc
zeb33n <zeb.bond@33n.co.uk>
2025-01-21 21:07:22 +0000

    added while loops

--- zeblang_wasm/src/interpret.rs
+++ zeblang_wasm/src/interpret.rs
@@ -1,35 +1,66 @@
 use std::collections::HashMap;
+use std::vec::IntoIter;
 
+use wasm_bindgen::UnwrapThrowExt;
 use zeblang::{ExpressionNode, StatementNode};
 
 struct Interpreter {
     vars: HashMap<String, i32>,
+    iter: IntoIter<StatementNode>,
+    out: Result<i32, String>,
 }
 
 pub fn interpret(parse_tree: Vec<StatementNode>) -> Result<i32, String> {
     return Interpreter {
         vars: HashMap::new(),
+        iter: parse_tree.into_iter(),
+        out: Err("Error".to_string()),
     }
-    .run(parse_tree);
+    .run();
 }
 
+// need to make a parse statement function we can call recursively
+// do we need to first translate everything into executable units that we then call in order
+// i think maybe so
 impl Interpreter {
-    fn run(&mut self, parse_tree: Vec<StatementNode>) -> Result<i32, String> {
-        let mut out = Err("Error".to_string());
-        for statement in parse_tree.into_iter() {
-            match statement {
-                StatementNode::Exit(node) => {
-                    out = self.interpret_exit(node);
-                    break;
-                }
-                StatementNode::Assign(name, node) => {
-                    let value = self.interpret_expr(node)?;
-                    self.vars.insert(name, value);
-                }
-                _ => todo!(),
-            }
+    fn run(&mut self) -> Result<i32, String> {
+        let mut statement_option = self.iter.next();
+        while statement_option.is_some() {
+            let statement = statement_option.ok_or("Some Looping went wrong!")?;
+            self.interpret_statement(statement)?;
+            statement_option = self.iter.next();
         }
-        return out;
+        return self.out.to_owned();
+    }
+
+    fn interpret_statement(&mut self, statement: StatementNode) -> Result<(), String> {
+        match statement {
+            StatementNode::Exit(node) => {
+                self.out = self.interpret_exit(node);
+                return Ok(());
+            }
+            StatementNode::Assign(name, node) => {
+                let value = self.interpret_expr(node)?;
+                self.vars.insert(name, value);
+            }
+            // get rid of clones rewrite with references
+            // nested loops not working?!
+            StatementNode::While(node) => {
+                let mut next = self.iter.next().ok_or("While loop not closed")?;
+                let mut stmnts: Vec<StatementNode> = Vec::new();
+                while next != StatementNode::EndWhile {
+                    stmnts.push(next);
+                    next = self.iter.next().ok_or("While loop not closed")?;
+                }
+                while self.interpret_expr(node.clone())? != 0 {
+                    for stmnt in stmnts.clone().into_iter() {
+                        self.interpret_statement(stmnt)?;
+                    }
+                }
+            }
+            _ => todo!(),
+        }
+        Ok(())
     }
 
     fn interpret_exit(&mut self, node: ExpressionNode) -> Result<i32, String> {
@@ -42,11 +73,9 @@ impl Interpreter {
             ExpressionNode::Value(value) => {
                 value.parse::<i32>().ok().ok_or("invalid int".to_string())
             }
-            ExpressionNode::Var(name) => Ok(self
-                .vars
-                .get(&name)
-                .ok_or("Undefined var".to_string())?
-                .to_owned()),
+            ExpressionNode::Var(name) => {
+                Ok(self.vars.get(&name).ok_or("Undefined var")?.to_owned())
+            }
             ExpressionNode::Infix(node1, infix, node2) => {
                 let v1 = self.interpret_expr(*node1)?;
                 let v2 = self.interpret_expr(*node2)?;
