409eee3011337b425d9e6e100003a129b1a2f1c0
zeb33n <zeb.bond@33n.co.uk>
2025-01-21 21:37:49 +0000

    taking references now

--- zeblang_wasm/src/interpret.rs
+++ zeblang_wasm/src/interpret.rs
@@ -1,19 +1,18 @@
 use std::collections::HashMap;
-use std::vec::IntoIter;
+use std::slice::Iter;
 
-use wasm_bindgen::UnwrapThrowExt;
 use zeblang::{ExpressionNode, StatementNode};
 
-struct Interpreter {
+struct Interpreter<'a> {
     vars: HashMap<String, i32>,
-    iter: IntoIter<StatementNode>,
+    iter: Iter<'a, StatementNode>,
     out: Result<i32, String>,
 }
 
 pub fn interpret(parse_tree: Vec<StatementNode>) -> Result<i32, String> {
     return Interpreter {
         vars: HashMap::new(),
-        iter: parse_tree.into_iter(),
+        iter: parse_tree.iter(),
         out: Err("Error".to_string()),
     }
     .run();
@@ -22,7 +21,7 @@ pub fn interpret(parse_tree: Vec<StatementNode>) -> Result<i32, String> {
 // need to make a parse statement function we can call recursively
 // do we need to first translate everything into executable units that we then call in order
 // i think maybe so
-impl Interpreter {
+impl<'a> Interpreter<'a> {
     fn run(&mut self) -> Result<i32, String> {
         let mut statement_option = self.iter.next();
         while statement_option.is_some() {
@@ -33,7 +32,7 @@ impl Interpreter {
         return self.out.to_owned();
     }
 
-    fn interpret_statement(&mut self, statement: StatementNode) -> Result<(), String> {
+    fn interpret_statement(&mut self, statement: &StatementNode) -> Result<(), String> {
         match statement {
             StatementNode::Exit(node) => {
                 self.out = self.interpret_exit(node);
@@ -41,19 +40,17 @@ impl Interpreter {
             }
             StatementNode::Assign(name, node) => {
                 let value = self.interpret_expr(node)?;
-                self.vars.insert(name, value);
+                self.vars.insert(name.to_owned(), value);
             }
-            // get rid of clones rewrite with references
-            // nested loops not working?!
             StatementNode::While(node) => {
                 let mut next = self.iter.next().ok_or("While loop not closed")?;
-                let mut stmnts: Vec<StatementNode> = Vec::new();
-                while next != StatementNode::EndWhile {
+                let mut stmnts: Vec<&StatementNode> = Vec::new();
+                while next != &StatementNode::EndWhile {
                     stmnts.push(next);
                     next = self.iter.next().ok_or("While loop not closed")?;
                 }
-                while self.interpret_expr(node.clone())? != 0 {
-                    for stmnt in stmnts.clone().into_iter() {
+                while self.interpret_expr(node)? != 0 {
+                    for stmnt in stmnts.iter() {
                         self.interpret_statement(stmnt)?;
                     }
                 }
@@ -63,22 +60,20 @@ impl Interpreter {
         Ok(())
     }
 
-    fn interpret_exit(&mut self, node: ExpressionNode) -> Result<i32, String> {
+    fn interpret_exit(&mut self, node: &ExpressionNode) -> Result<i32, String> {
         let value = self.interpret_expr(node);
         return value;
     }
 
-    fn interpret_expr(&mut self, node: ExpressionNode) -> Result<i32, String> {
+    fn interpret_expr(&mut self, node: &ExpressionNode) -> Result<i32, String> {
         return match node {
             ExpressionNode::Value(value) => {
                 value.parse::<i32>().ok().ok_or("invalid int".to_string())
             }
-            ExpressionNode::Var(name) => {
-                Ok(self.vars.get(&name).ok_or("Undefined var")?.to_owned())
-            }
+            ExpressionNode::Var(name) => Ok(self.vars.get(name).ok_or("Undefined var")?.to_owned()),
             ExpressionNode::Infix(node1, infix, node2) => {
-                let v1 = self.interpret_expr(*node1)?;
-                let v2 = self.interpret_expr(*node2)?;
+                let v1 = self.interpret_expr(node1)?;
+                let v2 = self.interpret_expr(node2)?;
                 match infix.as_str() {
                     "+" => Ok(v1 + v2),
                     "-" => Ok(v1 - v2),
