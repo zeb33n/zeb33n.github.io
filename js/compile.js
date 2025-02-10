de5172d5c176db93073c5070d9cdea47c4eca4c4
zeb33n <zeb.bond@33n.co.uk>
2025-02-10 18:08:44 +0000

    changed init

--- js/compile.js
+++ js/compile.js
@@ -1,12 +1,14 @@
 import init, {interpret_zeblang} from "../zeblang_wasm/pkg/zeblang.js";
 import {zext_dump_text} from "../zext/zext.js";
 
+// add waiting page for initialisation
+await init();
+
 function getText() {
   return zext_dump_text();
 }
 
 function hw() {
- init().then(() => {
    var stdout = document.getElementById("stdout");
    var stdexit = document.getElementById("stdexit")
    stdout.innerHTML = "";
@@ -14,7 +16,6 @@ function hw() {
    stdexit.innerHTML += "exit: ";
    console.log(getText());
    stdexit.innerHTML += interpret_zeblang(getText());
- });
 }
 
 document.getElementById("runBtn").addEventListener("click", hw)
